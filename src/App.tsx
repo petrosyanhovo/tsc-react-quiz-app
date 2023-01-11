dcf931cecebd5f8896be43389ea2a5a365b36b9f
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2023-01-11 16:56:18 +0400

    Added App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+// Components 
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer (e : React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
   return (
     <div className="App">
-      Quiz 
+      <h1>React Quiz</h1> 
+      <button className='start' onClick={startQuiz}>
+        Start
+      </button>
+      <p className='score'>Score : </p>
+      <p>Loading Questions ...</p>
+      <QuestionCard/>
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
